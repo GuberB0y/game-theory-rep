ced1fa8eab43418123cd05ee74eabf6f9138cd92
GuberB0y <131187547+GuberB0y@users.noreply.github.com>
2024-04-07 11:46:15 +0500

    first two derived classes / playGame functon
    Creating first two derived classes niceGuy and badGuy. playGame function
    also new data struct for specific players

--- players.cpp
+++ players.cpp
@@ -20,12 +20,50 @@ public:
         cout<<"Loses: "<<this->loses<<endl;
         cout<<"Ties: "<<this->ties<<endl;
     }
+    virtual int play(){}
+};
+//Creating a data struct for general statistics
+struct statsOfTheLastGame
+{
+    
 };
 
+//Creating derived niceGuy class  
+class niceGuy : defaultPlayer
+{
+public:
+    int play() override{return 0;}
+};
+//Creating derived badGuy class
+class badGuy : defaultPlayer
+{
+public:
+    int play() override{return 1;}
+};
+
+void playGame(defaultPlayer* p1, defaultPlayer* p2){
+    if (p1->play() > p2->play())
+    {
+        p1->addWins();
+        p2->addLoses();
+    }else if (p1->play() < p2->play())
+    {
+        p2->addWins();
+        p1->addLoses();
+    }else
+    {
+        p1->addTies();
+        p2->addTies();
+    }
+
+
+}
 
 
 int main(){
-    
+    defaultPlayer playerCheck;
+    playerCheck.addWins();
+    playerCheck.showStats();
 
     return 0;
 }
